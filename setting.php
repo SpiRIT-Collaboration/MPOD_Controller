2ae723d4aa307a3e2d500fbe2de494e7056b0260
Genie Jhang <geniejhang@majimak.com>
2014-11-08 21:19:00 +0900

    Bug fix on group selection load

--- setting.php
+++ setting.php
@@ -5,14 +5,14 @@
 //   Date: 2014. 11. 6
 //---------------------------------
 
-$filename = "settings.json";
+$filename = 'settings.json';
 $settingsText = file_get_contents($filename);
 $settings = json_decode($settingsText, true);
 
 if (isset($_GET['read'])) {
   $readItem = $_GET['read'];
 
-  $exist = array_key_exists($writeItem, $settings);
+  $exist = array_key_exists($readItem, $settings);
   if ($exist)
     echo $settings[$readItem];
 }
@@ -26,7 +26,7 @@ if (isset($_GET['write'])) {
     $settings[$writeItem] = $value;
   
   $settingsFile = fopen('settings.json', 'w+') or die("Unable to open file!");
-  fwrite($settingsFile, $settings);
+  fwrite($settingsFile, json_encode($settings));
   fclose($settingsFile);
 }
 
